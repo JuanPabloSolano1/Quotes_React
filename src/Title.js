580f26f1e652ed4b8852cd7c2553f9862cdac18d
juan solano <juanpablosolanosuarez@hotmail.com>
2020-03-17 12:28:29 +0100

    quotes_working

---
+++ src/Title.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Title = () => {
+  return <h1 className="title">Quote of the Day!</h1>;
+};
--- src/index.js
+++ src/index.js
@@ -1,12 +1,45 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import { render } from "react-dom";
+import { Button } from "./Button";
+import { Title } from "./Title";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class Quote extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      quote: "JP You are a rockstar",
+      author: "JP",
+      count: 0,
+      color: "Black"
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  handleClick() {
+    fetch(`https://type.fit/api/quotes`)
+      .then(response => response.json())
+      .then(data => {
+        this.setState({
+          quote: data[this.state.count].text,
+          count: this.state.count + 1,
+          author: data[this.state.count].author,
+          color: color[Math.floor(Math.random() * color.length)]
+        });
+      });
+  }
+  render() {
+    return (
+      <div className="container" style={{ backgroundColor: this.state.color }}>
+        <Title />
+        <h3 id="quote">"{this.state.quote}"</h3>
+        <p id="paragraph">-{this.state.author}-</p>
+        <div>
+          <Button onClick={this.handleClick} />
+        </div>
+      </div>
+    );
+  }
+}
+
+let color = ["#CD5C5C", "#F08080", "#FA8072", "#E9967A"];
+render(<Quote />, document.getElementById("root"));
