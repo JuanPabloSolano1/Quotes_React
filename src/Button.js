cbe8702c1d6bfb744c1619891a2d011c93a78209
juan solano <juanpablosolanosuarez@hotmail.com>
2020-03-17 14:25:47 +0100

    animations working

--- src/Button.js
+++ src/Button.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export const Button = props => {
   return (
-    <button className="btn btn-success" onClick={props.onClick}>
+    <button id="button"className="btn btn-success" onClick={props.onClick}>
       Next Quote!
     </button>
   );
